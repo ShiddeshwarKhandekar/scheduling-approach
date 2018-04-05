d5d52503fae3833a946e3e5fd5a305a5e4598b4d
ShiddeshwarKhandekar <shiddeshwarkhandekar@gmail.com>
2018-04-05 22:36:04 +0530

    p

--- priority_queue.c
+++ priority_queue.c
@@ -1,27 +1,75 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<>
+#include<stdbool.h>
 typedef struct node node;
 struct node{
         int data;
-        int priority;
+        //int priority;
         node* link;
 };
-node* newNode(int data, int priority){
-        node* temp = (node*)malloc(sizeof(node));
-        temp->data = data;
-        temp->priority = priority;
-        temp->link = NULL;
-        return temp;
+void newNode(node* p,int data ){
+        node* b = malloc(sizeof(node));
+        b->data = data;
+        b->link = p->link;
+        p->link = b;
+        //p = p->link;
 }
-void push(node** head,int data ,int priority){
-        node* start = (*head);
-        node* temp = newNode(data,priority);
-        if((*head)->priority > priority){
-                temp->link = (*head);
-                (*head) = temp
+void push(node** head,int data){
+        node *temp = *head;
+        if(data > (*head)->data )
+        {
+                //newNode(temp,data);
+                node* b = malloc(sizeof(node));
+                b->data = data;
+                b->link = temp;
+                (*head) = b;
         }
+        else{
+                        while((temp)->data > data){
+                                if(((temp)->link)->data > data){
+                                        (temp) = (temp)->link;
+                                }
+                                else{
+                                        break;
+                                }
+                        }
+                        if((temp) == NULL){
+                                node* b = malloc(sizeof(node));
+                                b->data = data;
+                                (temp) = b;
+                                b->link = NULL;
+                        }
+                        else{
+                                newNode((temp), data);
+                        }
+        }
+
 }
 int main(){
-
+        int c;
+        int element;
+        bool loop = true;
+        printf("Enter element \n");
+        //scanf("%d",&c);
+        node *start,*p;
+        p = malloc(sizeof(node));
+        p->data = -1;
+        p->link = NULL;
+        start = p;
+        while(loop){
+                scanf("%d",&element);
+                if(element == 0){
+                        loop = false;
+                }
+                //newNode(p, element);
+                else{
+                        push(&start,element);
+                }
+                //p = p->link;
+        }
+        p = start;
+        while(p->link != NULL){
+                printf("%d\n", p->data);
+                p = p->link;
+        }
 }
